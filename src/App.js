d81fe7b1be03138058baeccc30585b16fb748f81
marina <nicolaimarina@gmail.com>
2022-04-27 08:48:03 -0400

    installing react-router-dom@6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-function App() {
-  return <div className="App">Unsplash</div>;
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact component={Search} />
+        <Route path="/about" component={About} />
+        <Route path="/item/:id" component={Item} />
+      </Routes>
+    </Router>
+  );
+};
+
+function Search() {
+  return (
+    <div>
+      <nav>
+        <Link to="me">This is search</Link>
+      </nav>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <nav>
+        <Link to="me">This is about</Link>
+      </nav>
+    </div>
+  );
+}
+
+function Item() {
+  return (
+    <div>
+      <nav>
+        <Link to="me">This is item</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
